1f5bea59a94b44cb73e6036eba50ef26673e6912
mrigank44 <mriganksharma6787@gmail.com>
2026-01-25 19:22:10 +0530

    Update sumofarraydeclaringinside.java

--- sumofarraydeclaringinside.java
+++ sumofarraydeclaringinside.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 public class sumofarraydeclaringinside {
-    int sum=0,n=0,arr[] = new int[5];
+ 
     public static void main(String[] args){
+           int sum=0,n=0,arr[] = new int[5];
         Scanner s = new Scanner(System.in);
         sumofarraydeclaringinside obj = new sumofarraydeclaringinside();
         System.out.println("enter 5 numbers");
