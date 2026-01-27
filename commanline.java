993d826dba33e4e062d7d939a47b072e7898f74e
mrigank44 <mriganksharma6787@gmail.com>
2026-01-27 16:26:23 +0530

    added duplicate array program

---
+++ commanline.java
@@ -0,0 +1,9 @@
+class CommanLine {
+
+    public static void main(String[] args) {
+
+        System.out.println("First argument: " + args[0]);
+
+        System.out.println("Second argument: " + args[1]);
+    }
+}
---
+++ duplicates.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class duplicates {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of array");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    break;   // no need to check further
+                }
+            }
+        }
+
+        if (count > 0) {
+            System.out.println("Duplicate elements are present");
+        } else {
+            System.out.println("No duplicate elements are present");
+        }
+    }
+}
