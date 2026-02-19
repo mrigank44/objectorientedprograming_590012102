f0ef4952068b50144150542cf244566be2f19244
mrigank44 <mriganksharma6787@gmail.com>
2026-02-19 12:57:04 +0530

    append

---
+++ readandwrite.java
@@ -0,0 +1,59 @@
+//overwriting 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class readandwrite {
+
+    public static void main(String[] args) {
+
+        try {
+
+            
+            FileReader fr = new FileReader("sample.txt");
+            int i;
+            String oldContent = "";
+
+            System.out.println("Reading File Content:");
+
+            while ((i = fr.read()) != -1) {
+                System.out.print((char) i);
+                oldContent += (char) i;
+            }
+
+            fr.close();
+
+            System.out.println("\nFile read successfully.");
+
+            
+            System.out.println("\nContent before Update:");
+            System.out.println(oldContent);
+
+
+            
+            FileWriter fw = new FileWriter("sample.txt");
+
+            String newContent = "hello dc , updated line";
+
+            fw.write(newContent);
+            fw.close();
+
+            System.out.println("\nFile written successfully (Overwritten).");
+
+
+            
+            FileReader fr2 = new FileReader("sample.txt");
+
+            System.out.println("\nContent after Update:");
+
+            while ((i = fr2.read()) != -1) {
+                System.out.print((char) i);
+            }
+
+            fr2.close();
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
